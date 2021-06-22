49746a4d28e837a2cc3ebc18323936c545c15f92
jordanhailey <jordan.m.hailey@gmail.com>
2021-06-21 19:21:09 -0700

    creating Editor component

---
+++ src/lib/Editor.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let configuration = undefined;
+  /**
+   * @param node HTMLElement chained with the [`use:action` Element directive](https://svelte.dev/docs#use_action)
+   * @param config Action parameters with app assigned configuration
+   */
+  function editorFunctionality(node,config) {
+    console.log("TODO: Initialize Editor",{node,config});
+  }
+</script>
+<div use:editorFunctionality={configuration} id="editor-container">
+  Editor Container
+</div>
