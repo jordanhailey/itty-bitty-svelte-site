8da025293744b0490df975dd634f4973c4b41da1
jordanhailey <jordan.m.hailey@gmail.com>
2021-06-21 19:20:38 -0700

    passing context to App

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,22 @@
-<script lang="ts">
-import App from "$lib/App.svelte";
+<script lang="ts" context="module">
+  import type {Load} from "@sveltejs/kit";
+  export const load:Load = async({context}) => {
+    const output = {props:{loadingCtx:{inflate:undefined}}}
+    try {
+      if (context.err) throw context.err
+      output.props.loadingCtx.inflate = context.inflate
+      // Check context for certain props and do something before rendering
+    } catch (err) {
+      // Handle errors from
+      console.error(err);
+    } finally {
+      return output;
+    }
+  };
 </script>
-
-<App />
+<script lang="ts">
+  import App from "$lib/App.svelte";
+  export let loadingCtx = undefined;
+</script>
+/index route
+<App state={loadingCtx}/>
